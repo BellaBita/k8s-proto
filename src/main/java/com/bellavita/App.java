57649c0ecc46ddbc4c2076d4fef14f59670b6331
shKim <bechco@gmail.com>
2019-12-29 21:51:34 +0900

    1st

---
+++ src/main/java/com/bellavita/App.java
@@ -0,0 +1,13 @@
+package com.bellavita;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+        
+    }
+}
